9ddba6b8c134f05063bea41b48e2a1bfb2da3b3f
Maxim Biro <nurupo.contributions@gmail.com>
2015-07-27 23:27:31 -0400

    Made logging fancier, probably too much
    Now logs look like:
    
    [HH:MM:SS] (namespace::class::function)
    -- This is 1st log message.
    
    [HH:MM:SS] (namespace::class::function)
    -- This is 2nd log message.
    
    The time is probably unnecessary, but oh well.
    Function names are wrapped in "()" for clarity -- it's easier to eyeball them that way.
    Same for "--" and the empty line bentween log entries.

--- src/utils.h
+++ src/utils.h
@@ -2,10 +2,31 @@
 #define UTILS_H
 
 #ifdef WEBCAM_CAPTURE_DEBUG
-#include <iostream>
-#define DEBUG_PRINT(msg) std::cerr << msg << std::endl << std::flush
+
+    // apparently VC2013 doesn't support C++11's __func__
+    #if defined(__FUNCTION__)
+        #define FUNCTION __FUNCTION__
+    #elif defined(__FUNC__)
+        #define FUNCTION __FUNC__
+    #else
+        #define FUNCTION "function-name-unavailable"
+    #endif
+
+        #include <chrono>
+        #include <iomanip>
+        #include <iostream>
+
+        #define DEBUG_PRINT(msg) \
+                { \
+                    auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()); \
+                    std::cerr << "[" << std::put_time(std::localtime(&now), "%H:%M:%S") << "] " \
+                              << "(" << FUNCTION  << ")" << std::endl \
+                              << "-- " << msg \
+                              << std::endl << std::endl \
+                              << std::flush; \
+                }
 #else
-#define DEBUG_PRINT(msg)
+    #define DEBUG_PRINT(msg)
 #endif
 
 #define FPS_FROM_RATIONAL(x, y) (static_cast<float>(x)/(y))
